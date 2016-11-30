e3ffbbb05d7c96aac9e7646a07f87a60f082ad3e
gruppjo <jona_blubb@web.de>
2016-11-30 17:37:07 +0100

    homeowner.component - initial implementation with ngForms

--- src/app/homeowner/homeowner.component.ts
+++ src/app/homeowner/homeowner.component.ts
@@ -1,12 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AppState } from '../app.service';
+
+export interface DarlehensDaten {
+  darlehen: number;
+  inflation: number;
+  sollzins: number;
+  tilgung: number;
+  laufzeit: number;
+}
+
 @Component({
+  providers: [
+    AppState
+  ],
   selector: 'ho-homeowner',
-  styleUrls: [ './homeowner.component.css'],
-  templateUrl: './homeowner.component.html'
+  styleUrls: [ 'homeowner.component.css'],
+  templateUrl: 'homeowner.component.html'
 })
 export class HomeownerComponent implements OnInit {
-  constructor() { }
+  private verbose: boolean = true;
+  private input: DarlehensDaten = {
+    darlehen: 350000,
+    inflation: 1.5,
+    sollzins: 1.5,
+    tilgung: 2.0,
+    laufzeit: 10
+  };
+
+  get diagnostic() {
+    return JSON.stringify(this.input, null, 2);
+  }
+  constructor(private appState: AppState) { }
 
   ngOnInit() { }
+
+  onSubmit () {
+    console.log('submitted');
+  }
+  toggleVerbose(): boolean {
+    return this.verbose = !this.verbose;
+  }
 }
